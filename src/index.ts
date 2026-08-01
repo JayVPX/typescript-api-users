152a538bc137a23f5b1b43700023b9d205c4d70e
João Costa Praxedes <joaocostapraxedes@gmail.com>
2026-08-01 16:37:45 -0300

    feat: Starting Express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log("hello world! ");
+import express from "express";
+import { config } from "dotenv";
+
+config();
+
+const app = express();
+const port = process.env.PORT;
+
+app.get("/", (req, res) => {
+  res.send("BOM DIA MENINA");
+});
+
+app.listen(port, () => {
+  console.log(`Listening on ${port}`);
+});
