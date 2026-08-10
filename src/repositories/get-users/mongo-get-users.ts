4cfafb2550b2b3f1d8b8ee61ff318e23f9bd6f5f
João Costa Praxedes <joaocostapraxedes@gmail.com>
2026-08-10 17:11:13 -0300

    feat: MongoGetUsersRepository getting users from mongodb

--- src/repositories/get-users/mongo-get-users.ts
+++ src/repositories/get-users/mongo-get-users.ts
@@ -1,15 +1,17 @@
 import type { IGetUserRepository } from "../../controllers/get-users/protocols.js";
+import { MongoClient } from "../../database/mongo.js";
 import type { User } from "../../models/user.model.js";
 
 export class MongoGetUsersRepository implements IGetUserRepository {
   async getUsers(): Promise<User[]> {
-    return [
-      {
-        email: "joao@gmail.com",
-        firstName: "João",
-        lastName: "Praxedes",
-        password: "123456",
-      },
-    ];
+    const users = await MongoClient.db
+      .collection<Omit<User, "id">>("users")
+      .find({})
+      .toArray();
+
+    return users.map(({ _id, ...rest }) => ({
+      ...rest,
+      id: _id.toHexString(),
+    }));
   }
 }
